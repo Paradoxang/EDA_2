8f7d5d770ec768a4c6ec59c5569c78a9064d0fe5
Paradox <eldfanga@gmail.com>
2024-10-23 16:34:24 -0500

    Se crea los dos primeros challenges del Corte 2

---
+++ Challenges Corte 2/01-react/src/FirstApp.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+ 
+const FirstApp = () => {
+    return (
+        <div>
+            <h1> Counter </h1>
+            <span> 10 </span>
+        </div>
+    )
+}
+export default FirstApp
---
+++ Challenges Corte 2/02-react/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react';
+import FirstApp from './FirstApp';  // Importamos el componente FirstApp
+
+const App = () => {
+  return (
+    <div>
+      <FirstApp />  {/* Desplegamos el componente */}
+    </div>
+  );
+};
+
+export default App
