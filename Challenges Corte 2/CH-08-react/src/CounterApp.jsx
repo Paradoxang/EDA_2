c2c85d741272f7394f5e829cc5596a35923a1fb7
Paradoxang <eldfanga@gmail.com>
2024-10-30 15:13:15 -0500

    Se crea el archivo de información para el nombre

---
+++ Challenges Corte 2/CH-08-react/src/CounterApp.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const CounterApp = () => {
+
+  const [counter, setCounter] = useState(10);
+
+  // Función para sumar 1 al contador
+  const handleAdd = () => {
+    setCounter(counter + 1);
+  };
+
+  // Función para restar 1 al contador, pero que no sea menor de 0
+  const handleSubstract = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
+  // Función para reiniciar el contador al valor por defecto (10)
+  const handleReset = () => {
+    setCounter(10);
+  };
+
+  return (
+    <div>
+      <h1>Contador: {counter}</h1> {/* Mostrar el valor del contador */}
+
+      {/* Botón para sumar */}
+      <button onClick={handleAdd}>Sumar</button>
+
+      {/* Botón para restar, con validación para no permitir valores negativos */}
+      <button onClick={handleSubstract}>Restar</button>
+
+      {/* Botón para reiniciar */}
+      <button onClick={handleReset}>Reiniciar</button>
+    </div>
+  );
+};
+
+export default CounterApp;
