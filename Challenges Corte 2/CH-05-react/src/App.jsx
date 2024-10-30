998cb6f4fb1fe49a835fb4b9a0eeb123b0fd25fa
Paradoxang <eldfanga@gmail.com>
2024-10-30 14:56:51 -0500

    Se crea el poke api fetcher en CH05

---
+++ Challenges Corte 2/CH-05-react/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react';
+import MultipleCustomHooks from './MultipleCustomHooks';
+
+const App = () => {
+  return (
+    <div>
+      <MultipleCustomHooks />
+    </div>
+  );
+};
+
+export default App;
